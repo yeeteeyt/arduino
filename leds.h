bdab1fedbda1b35c04964b584664955a75292231
yeetee <77510164+yeeteeyt@users.noreply.github.com>
2022-03-07 14:55:58 -0500

    this is a disaster
    attempt at flashing led

--- leds.h
+++ leds.h
@@ -1,1 +1,42 @@
 
+void setup(){ 
+pinMode(13,OUTPUT);
+pinMode(0,OUTPUT);
+pinMode(1,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+
+
+digitalWrite(13,LOW);
+
+        }
+void loop() {
+digitalWrite(0,LOW);
+delay(100);
+digitalWrite(1,LOW);
+delay(100);
+digitalWrite(2,LOW);
+delay(10);
+digitalWrite(0,LOW);
+delay(10);
+digitalWrite(1,LOW);
+delay(10);
+digitalWrite(2,LOW);
+delay(10);
+
+digitalWrite(0,HIGH);
+delay(100);
+digitalWrite(1,HIGH);
+delay(100);
+digitalWrite(2,HIGH);
+delay(10);
+digitalWrite(0,HIGH);
+delay(10);
+digitalWrite(1,HIGH);
+delay(10);
+digitalWrite(2,HIGH);
+delay(10);
+
+
+
+}
